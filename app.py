6ee26b7a3ae3ff72bb88a8c015816df69116282e
vtablas001 <victor.tablas@gmail.com>
2026-04-12 13:22:06 -0600

    updates to app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 import cv2
 from huggingface_hub import from_pretrained_keras
+import pandas as pd
 
 st.header("Tooth detection and segmentation in panoramic X-Rays")
 st.subheader("Iteration to improve demo")
@@ -81,49 +82,6 @@ with col3:
 ## If we have an image to input into the model, 
 ## we process it and feed it to the model
 if archivo_imagen is not None:
-    ## Load the image with PIL, display it, and convert it to a NumPy array
-    img = Image.open(archivo_imagen)
-    st.image(img, width=850)
-    img = np.array(img)    #Creates a writable copy
-
-    ## Process the image for model input
-    img_cv = convertir_one_channel(img)
-    img_cv = cv2.resize(img_cv, (512, 512), interpolation=cv2.INTER_LANCZOS4)
-    img_cv = np.float32(img_cv / 255)
-    img_cv = np.reshape(img_cv, (1, 512, 512, 1))
-
-    ## Feed the NumPy array into the model
-    predicted = model.predict(img_cv)
-    predicted = predicted[0]
-
-    ## Resize the image back to its original shape and add the segmentation masks
-    predicted = cv2.resize(
-        predicted, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_LANCZOS4)
-    
-    mask = np.uint8(predicted * 255)
-    _, mask = cv2.threshold(
-        mask, thresh=0, maxval=255, type=cv2.THRESH_BINARY + cv2.THRESH_OTSU
-    )
-    
-    kernel = np.ones((5, 5), dtype=np.float32)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
-    
-    cnts, hieararch = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    output = cv2.drawContours(convertir_one_channel(img), cnts, -1, (255, 0, 0), 3)
-
-    ## If we successfully obtained a result, display it in the interface
-    if output is not None:
-        st.subheader("Image segmentation:")
-        st.write(output.shape)
-        st.image(output, width=850)
-
-
-    ## This will provide a dashboard
-    ## If we successfully obtained a result, display it in the interface
-    ## If we have an image to input into the model, 
-## we process it and feed it to the model
-if archivo_imagen is not None:
     ## Load the image with PIL and display it immediately
     img = Image.open(archivo_imagen)
     st.image(img, width=850)
@@ -188,6 +146,5 @@ if archivo_imagen is not None:
                 "Height (px)": h
             })
             
-        import pandas as pd
         df_dientes = pd.DataFrame(datos_dientes)
         st.dataframe(df_dientes, use_container_width=True)
